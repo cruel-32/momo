458671386ebe7ab258f3dd45986f9f917d9b5a52
ITO_MZ_CHOESEUN\mz_choeseunghui <mz_choeseunghui@woowafriends.com>
2023-03-24 08:51:01 +0900

    테스트코드 작성과 셋팅

--- src/components/__test__/Test.test.tsx
+++ src/components/__test__/Test.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { RecoilRoot, useRecoilValue } from 'recoil';
 import { describe, expect, it, vi } from 'vitest';
 
@@ -7,28 +7,26 @@ import currentUserState from '@/stores/users';
 import { render } from '@/tests/render';
 
 describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
-
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
+  const props = {
+    onChange: vi.fn(),
+  };
 
   it('리액트 렌더링 테스트', async () => {
-    const props = {
-      onChange: vi.fn(),
-    };
+    const { container } = render(<Test {...props} />);
+    const el = container.querySelector('[name=currentUserName]');
+    expect(el).not.toBeNull();
+  });
 
-    act(() => render(<Test {...props} />));
-
-    fireEvent.change(screen.getByTestId('currentUserName'), {
-      target: { value: 'user@test.com' },
-    });
-
-    // const value = useRecoilValue(currentUserState);
-
+  it('동작 테스트', () => {
+    const { container } = render(<Test {...props} />);
+    const el = container.querySelector('[name=currentUserName]');
+    if (el !== null) {
+      fireEvent.change(el, {
+        target: { value: 'user@test.com' },
+      });
+    }
     expect(props.onChange).toHaveBeenCalled();
+    // recoil 상태값을 어떻게 테스트 할 것인가
     expect(screen.getByTestId('target').innerHTML).toBe('user@test.com');
   });
 });
